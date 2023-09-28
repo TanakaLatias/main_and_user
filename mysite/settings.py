74f4291b37b5d5b0b6ba25385df8fe584c6e9153
TanakaLatias <109828366+TanakaLatias@users.noreply.github.com>
2023-09-28 13:43:01 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,8 +87,7 @@ DATABASES = {
 """
 import dj_database_url
 import os
-#default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
-DATABASES = {'default': dj_database_url.config(default=default_dburl, conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
