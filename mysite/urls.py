827928eecc56658c8722581c2588fedfefe7f99a
TanakaLatias <109828366+TanakaLatias@users.noreply.github.com>
2023-09-28 13:24:38 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('main_and_user.urls')),
+    path('', include('main_and_user.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
