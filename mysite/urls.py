25c4052992d346fe082e56c67d1ea7e91c247135
TanakaLatias <109828366+TanakaLatias@users.noreply.github.com>
2023-09-28 19:50:58 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_and_user.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
